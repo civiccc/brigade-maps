a3112872e5b793a8669caa41de4ae5be9b6cbba0
Noah Silas <noahking@gmail.com>
2016-04-04 11:40:34 -0700

    Improve cloudinary upload reliability
    - Don't attempt to upload tiles without an ocd-id. These tiles would
      never appear in the product anyways.
    
    - Limit upload concurrency to 20 simultaneous uploads. This makes the
      script take longer, but avoids having random tiles fail to upload.

--- bin/upload.js
+++ bin/upload.js
@@ -1,19 +1,34 @@
+var async = require('async');
 var cloudinary = require('cloudinary');
 var fs = require('fs');
 var path = require('path');
 
+var uploadQueue = async.queue(
+  function(tile, callback) {
+    console.log('uploading ' + tile.ocdid + '/' + tile.level);
+    var tilePath = path.join('build', tile.ocdid, tile.level + '.png');
+    cloudinary.uploader.upload(
+      tilePath,
+      function(result) {
+        if (result.public_id == undefined) {
+          console.log("Failed to upload tile " + tilePath + " : " + result)
+        } else {
+          console.log('uploaded ' + tile.ocdid + '/' + tile.level);
+        }
+        callback();
+      },
+      {
+        public_id: tile.ocdid + '/' + tile.level,
+        tags: ['district', 'district-' + tile.level]
+      }
+    );
+  },
+  20 // max concurrent jobs
+)
+
 var tiles = JSON.parse(fs.readFileSync('build/tiles.json'));
 
 tiles.forEach(function(tile) {
-  var tilePath = path.join('build', tile.ocdid, tile.level + '.png');
-  console.log('uploading ' + tile.ocdid + '/' + tile.level);
-
-  cloudinary.uploader.upload(
-    tilePath,
-    function(result) {},
-    {
-      public_id: tile.ocdid + '/' + tile.level,
-      tags: ['district', 'district-' + tile.level]
-    }
-  );
+  if (tile.ocdid == undefined) { return }
+  uploadQueue.push(tile);
 });
