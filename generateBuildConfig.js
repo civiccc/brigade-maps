c47a895a66f816ed3a013bc82fb951949016b956
Noah Silas <noahking@gmail.com>
2016-04-05 16:01:30 -0700

    Skip render for ocdid-less tiles
    These won't be uploaded or appear in the product, so speed up the
    rendering by skipping tiles that don't have an ocdid mapping

--- generateBuildConfig.js
+++ generateBuildConfig.js
@@ -7,6 +7,11 @@ var glob = require('glob');
 
 mapnik.register_default_input_plugins();
 
+const HIGHLIGHT_COLORS = {
+  federal: '#ffaf50',
+  state: '#0196b4',
+  local: '#3dc489'
+}
 
 // Globals
 // map configuration
@@ -14,10 +19,10 @@ var config = JSON.parse(fs.readFileSync('config/maps.json'));
 // ocdids, indexed for fast lookup
 var ocdidmaps = {}
 
-var ocdIdKey = function(attributes) {
-  return attributes.join('.')
-}
+// helper to transform the attributes from config['render_each'] into a hash key
+var ocdIdKey = (attributes) => attributes.join('.')
 
+// builds a hash from the ocdid_mapping file for quick ocdid lookups
 var generateOcdIdMaps = function(mapName, config) {
   var mappingPath = path.join('config', mapName, 'ocdid_mapping.csv')
   if (!fs.existsSync(mappingPath)) {
@@ -33,13 +38,12 @@ var generateOcdIdMaps = function(mapName, config) {
   }
 }
 
+// looks up an ocdid from its 'render each' attributes
 var getTileOCDID = function(renderAttributes, mapName) {
-  if (mapName in ocdidmaps) {
-    return ocdidmaps[mapName][ocdIdKey(renderAttributes)]
-  }
-  return null;
+  return ocdidmaps[mapName][ocdIdKey(renderAttributes)]
 }
 
+// check if a feature matches any of the attributes in the skip list
 var shouldSkip = function(config, feature) {
   if (config['skip']) {
     var datum = feature.attributes();
@@ -72,30 +76,23 @@ Object.keys(config).forEach(function(map) {
 
     while (feature = featureset.next()) {
 
-      if (shouldSkip(config[map], feature)) {
-        continue;
-      }
+      if (shouldSkip(config[map], feature)) { continue; }
 
       var datum = feature.attributes()
-      var renderAttributes = []
+      var renderAttributes = config[map]['render_each'].map((attr) => datum[attr])
 
-      datum.shapefile = shapefile;
-
-      config[map]['render_each'].forEach(function(i) {
-        renderAttributes.push(datum[i])
-      });
-
+      // skip rendering of tiles without an ocdid, since we won't represent
+      // them on brigade anywhere
       var ocdid = getTileOCDID(renderAttributes, map, config[map]);
+      if (!ocdid) { continue}
+
       var tileName = renderAttributes.join("-")
 
       for (level of config[map]['levels']) {
         var xmlPath = buildConfigDir + "/" + tileName + '_' + level + '.xml';
 
-        datum.highlightColor = {
-          federal: '#ffaf50',
-          state: '#0196b4',
-          local: '#3dc489'
-        }[level]
+        datum.highlightColor = HIGHLIGHT_COLORS[level]
+        datum.shapefile = shapefile;
 
         fs.writeFileSync(xmlPath,
           swig.renderFile('config/' + map + '/stylesheet.xml.swig', datum)
