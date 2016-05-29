0d3d8fdfb7268f416fbb690d7d332805d0e1c9a7
Tom Dooner <tom.dooner@brigade.com>
2016-05-29 13:56:35 -0700

    Update deploy script to handle GITHUB_AUTH_TOKEN secret

--- renderAndPush.sh
+++ renderAndPush.sh
@@ -11,10 +11,18 @@ if ! git diff --cached --quiet; then
   exit 1
 fi
 
+if [ ! -d .git ]; then
+  git init
+  git config user.name "Travis-CI Deploy Bot"
+  git config user.email "tomdooner@gmail.com"
+fi
+
+# render everything
 npm run render
+
 git branch -D gh-pages 2>/dev/null || true
 git checkout -b gh-pages
 mv build/ocd-division .
 git add ocd-division
 git commit --quiet -m "Generated build at $(date)"
-git push origin gh-pages
+git push --force "https://x-api-token:${GITHUB_AUTH_TOKEN}@github.com/tdooner/brigade-maps.git" gh-pages
