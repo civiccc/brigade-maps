89f041c16184895eec84c56c1cf6438f53eddc3b
Tom Dooner <tom.dooner@brigade.com>
2016-08-12 21:57:16 +0000

    Extract shapefile iteration logic into "mapConfig" module

--- generateBuildConfig.js
+++ generateBuildConfig.js
@@ -1,8 +1,11 @@
+'use strict';
+
 const fs = require('fs');
-const glob = require('glob');
+
 const mapnik = require('mapnik');
 const swig = require('swig');
 
+const buildConfig = require('./lib/buildConfig');
 const ocdidMappingProcessor = require('./lib/ocdidMappingProcessor');
 
 mapnik.register_default_input_plugins();
@@ -13,82 +16,49 @@ const HIGHLIGHT_COLORS = {
   local: '#3dc489'
 };
 
-// Globals
-// map configuration
-const config = JSON.parse(fs.readFileSync('config/maps.json'));
-
 // check if a feature matches any of the attributes in the skip list
-const shouldSkip = function(mapConfig, feature) {
-  if (mapConfig.skip) {
-    const datum = feature.attributes();
-
-    for (const attr of Object.keys(mapConfig.skip)) {
-      if (datum[attr] === mapConfig.skip[attr]) {
-        return true;
-      }
-    }
-  }
-  return false;
-};
-
 // output collection
 const tiles = [];
 
-Object.keys(config).forEach(function(map) {
-  const buildConfigDir = 'build/' + map;
+buildConfig().eachMap((mapName, mapConfig) => {
+  const buildConfigDir = 'build/' + mapName;
   console.log('Generating config ' + buildConfigDir);
   if (!fs.existsSync(buildConfigDir)) {
     fs.mkdirSync(buildConfigDir);
   }
 
-  ocdidMappingProcessor.generateOcdIdMaps(map, config[map]);
+  ocdidMappingProcessor.generateOcdIdMaps(mapName, mapConfig);
 
-  const shapefiles = glob.sync(config[map].shapefile);
-  const overrides = config[map].overrides || {};
+  mapConfig.eachFeature((feature, shapefile) => {
+    const datum = feature.attributes();
+    const renderAttributes = mapConfig.render_each.map((attr) => datum[attr]);
+    const tileName = renderAttributes.join('-');
 
-  for (const shapefile of shapefiles) {
-    const featureset = new mapnik.Datasource({ type: 'shape', file: shapefile }).featureset();
-    let feature;
-
-    // eslint-disable-next-line
-    while (feature = featureset.next()) {
-
-      if (shouldSkip(config[map], feature)) {
-        continue;
-      }
-
-      const datum = feature.attributes();
-      const renderAttributes = config[map].render_each.map((attr) => datum[attr]);
-
-      // skip rendering of tiles without an ocdid, since we won't represent
-      // them on brigade anywhere
-      const ocdid = ocdidMappingProcessor.getTileOCDID(renderAttributes, map, config[map]);
-      if (!ocdid) {
-        continue;
-      }
-
-      const extent = overrides[ocdid] || feature.extent();
-      const tileName = renderAttributes.join('-');
-
-      for (const level of config[map].levels) {
-        const xmlPath = buildConfigDir + '/' + tileName + '_' + level + '.xml';
-
-        datum.highlightColor = HIGHLIGHT_COLORS[level];
-        datum.shapefile = shapefile;
-
-        fs.writeFileSync(xmlPath,
-          swig.renderFile('config/' + map + '/stylesheet.xml.swig', datum)
-        );
-
-        tiles.push({
-          xmlPath: xmlPath,
-          ocdid: ocdid,
-          level: level,
-          extent: extent
-        });
-      }
+    // skip rendering of tiles without an ocdid, since we won't represent
+    // them on brigade anywhere
+    const ocdid = ocdidMappingProcessor.getTileOCDID(renderAttributes, mapName);
+    if (!ocdid) {
+      return;
     }
-  }
+
+    for (const level of mapConfig.levels) {
+      const xmlPath = buildConfigDir + '/' + tileName + '_' + level + '.xml';
+
+      datum.highlightColor = HIGHLIGHT_COLORS[level];
+      datum.shapefile = shapefile;
+
+      fs.writeFileSync(xmlPath,
+        swig.renderFile('config/' + mapName + '/stylesheet.xml.swig', datum)
+      );
+
+      tiles.push({
+        xmlPath: xmlPath,
+        ocdid: ocdid,
+        level: level,
+        extent: mapConfig.extent(feature, ocdid),
+      });
+    }
+  });
 });
 
 fs.writeFileSync('build/tiles.json', JSON.stringify(tiles));
